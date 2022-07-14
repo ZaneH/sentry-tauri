5c2751cc32313b5eac57699ff6b07568d2ab6eed
Zane Helton <zane99@me.com>
2022-07-14 18:30:09 -0400

    Add plugin-store and read is-sentry-on before init

--- js/inject.ts
+++ js/inject.ts
@@ -1,5 +1,6 @@
 import * as Sentry from "@sentry/browser";
 import { defaultOptions } from "./";
+import { Store } from "tauri-plugin-store-api";
 
 declare var __DEBUG__: boolean;
 
@@ -9,7 +10,23 @@ declare global {
   }
 }
 
-window.Sentry = Sentry;
+new Store(".settings.dat").get("is-sentry-on").then((isSentryOn) => {
+    if (!Boolean(isSentryOn)) {
+        return;
+    }
+
+    Sentry.init({
+        // We don't send from the browser but a DSN is required for the SDK to work
+        dsn: "https://123456@dummy.dsn/0",
+        // We don't want to track browser sessions
+        autoSessionTracking: false,
+        // We replace this with true or false before injecting this code into the browser
+        debug: __DEBUG__
+    });
+
+    window.Sentry = Sentry;
+});
+
 
 Sentry.init({
   ...defaultOptions,
