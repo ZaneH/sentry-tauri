c40ee7ef3f0ad0ea51eff05807061b52baf1ade1
Zane Helton <zane99@me.com>
2025-07-03 02:16:17 -0400

    Update with new changes from upstream

--- dist/inject.js
+++ dist/inject.js
@@ -1,8 +1,16 @@
 import * as Sentry from '@sentry/browser';
 import { defaultOptions } from './';
-window.Sentry = Sentry;
-Sentry.init({
-    ...defaultOptions,
-    // We replace this with true or false before injecting this code into the browser
-    debug: __DEBUG__,
-});
+import { Store } from "@tauri-apps/plugin-store";
+Store.load(".settings.dat").then((store => {
+    store.get("is-sentry-on").then((isSentryOn) => {
+        if (!Boolean(isSentryOn)) {
+            return;
+        }
+        Sentry.init({
+            ...defaultOptions,
+            // We replace this with true or false before injecting this code into the browser
+            debug: __DEBUG__,
+        });
+        window.Sentry = Sentry;
+    });
+}));
--- js/inject.ts
+++ js/inject.ts
@@ -1,5 +1,6 @@
 import * as Sentry from '@sentry/browser';
 import { defaultOptions } from './';
+import { Store } from "@tauri-apps/plugin-store";
 
 declare const __DEBUG__: boolean;
 
@@ -9,10 +10,18 @@ declare global {
   }
 }
 
-window.Sentry = Sentry;
+Store.load(".settings.dat").then((store => {
+  store.get("is-sentry-on").then((isSentryOn) => {
+    if (!Boolean(isSentryOn)) {
+      return;
+    }
 
-Sentry.init({
-  ...defaultOptions,
-  // We replace this with true or false before injecting this code into the browser
-  debug: __DEBUG__,
-});
+    Sentry.init({
+      ...defaultOptions,
+      // We replace this with true or false before injecting this code into the browser
+      debug: __DEBUG__,
+    });
+
+    window.Sentry = Sentry;
+  })
+}))
